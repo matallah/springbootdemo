f3101708fab5350424c33ec8edbf1c92b2890920
mohamed.atallah <mohamed.atallah@asset.com.eg>
2020-08-04 12:12:31 +0200

    heloo

--- src/main/java/com/example/demo/BasicController.java
+++ src/main/java/com/example/demo/BasicController.java
@@ -1,14 +1,14 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+@Controller
 public class BasicController {
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String index() {
-        return "Greetings from Setooooooooohy11111111112222222wwwwwwwww!";
+        return "index";
     }
 
 }
